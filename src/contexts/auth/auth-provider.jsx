3123df05233f43dafe65682461d0f0cfe83f2666
Michael Nascimento <michael.nsc@outlook.com>
2023-08-03 08:57:12 -0300

    refatora auth-provider

--- src/contexts/auth/auth-provider.jsx
+++ src/contexts/auth/auth-provider.jsx
@@ -1,51 +1,57 @@
 /* eslint-disable react/prop-types */
 import { useEffect, useRef, useState } from "react";
 import { useMatch } from "react-router-dom";
-import auth from "../../services/auth-service";
+import { useLogger } from "../../hooks/use-logger";
+import { clearSession, getCurrentSession, oauthLogin } from "../../services/auth-service";
 import { AuthContext } from "./auth-context";
 
 export const AuthProvider = ({ children }) => {
+  const log = useLogger("AuthProvider");
   const isLoginPage = !!useMatch("login");
-  console.log(isLoginPage);
+
   const [loading, setLoading] = useState(true);
   const sessionId = useRef("");
   const isLoggedIn = !!sessionId.current;
 
-  const getUserSession = async () => {
-    try {
-      setLoading(true);
-      const session = await auth.getSession("current");
-      sessionId.current = session.$id;
-    } catch (error) {
-      console.log(error);
-    }
-    setLoading(false);
-  };
-
   const logout = async () => {
     sessionId.current = "";
     setLoading(true);
-    await auth.deleteSession(sessionId.current);
-    setLoading(false);
+
+    try {
+      await clearSession();
+    } catch (error) {
+      log.error(error, { caller: 'logout' });
+    } finally {
+      setLoading(false);
+    }
   };
 
   const loginWithGoogle = async () => {
-    auth.createOAuth2Session(
-      "google",
-      "http://localhost:5173",
-      "http://localhost:3000/auth-error",
-      [
-        "https://www.googleapis.com/auth/userinfo.email",
-        "https://www.googleapis.com/auth/userinfo.profile",
-      ]
-    );
+    try {
+      oauthLogin()
+    } catch (error) {
+      log.error(error, { caller: 'loginWithGoogle' });
+    }
   };
 
   useEffect(() => {
     if (!isLoginPage) {
+
+      const getUserSession = async () => {
+        setLoading(true);
+        try {
+          const session = await getCurrentSession();
+          sessionId.current = session.$id;
+        } catch (error) {
+          log.error(error, { caller: 'useEffect > getUserSession' });
+        } finally {
+          setLoading(false);
+        }
+      };
+
       getUserSession();
     }
-  }, [isLoginPage]);
+  }, [isLoginPage, log]);
 
   return (
     <AuthContext.Provider

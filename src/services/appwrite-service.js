a2da15760c4c09f44b4336c6236d5e4b6e30cb8a
Michael Nascimento <michael.nsc@outlook.com>
2023-08-03 08:58:01 -0300

    remove console.log

--- src/services/appwrite-service.js
+++ src/services/appwrite-service.js
@@ -1,6 +1,6 @@
 import { Client } from "appwrite";
 import { ENV } from "../config/constants";
-console.log(ENV);
+
 const client = new Client();
 client.setEndpoint(ENV.ENDPOINT).setProject(ENV.PROJECT_ID);
 

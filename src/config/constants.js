aa7f17699eea844fb031f80b7ddfcf480fd3014c
Michael Nascimento <michael.nsc@outlook.com>
2023-08-03 08:56:40 -0300

    adiciona env is_development

--- src/config/constants.js
+++ src/config/constants.js
@@ -3,4 +3,5 @@ export const ENV = {
   PROJECT_ID: import.meta.env.MK_PROJECT_ID,
   DATABASE_ID: import.meta.env.MK_DATABASE_ID,
   COLLECTION_ID: import.meta.env.MK_COLLECTION_ID,
+  IS_DEVELOPMENT: import.meta.env.NODE_ENV === "development",
 };

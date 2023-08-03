a1522e3b4a53a25d1a3616b9d887d6d2593a7bf0
Michael Nascimento <michael.nsc@outlook.com>
2023-08-03 09:15:26 -0300

    ajuste de variaveis de ambiente

--- src/config/constants.js
+++ src/config/constants.js
@@ -4,4 +4,6 @@ export const ENV = {
   DATABASE_ID: import.meta.env.MK_DATABASE_ID,
   COLLECTION_ID: import.meta.env.MK_COLLECTION_ID,
   IS_DEVELOPMENT: import.meta.env.NODE_ENV === "development",
+  ROLLBAR_ACCESS_TOKEN: import.meta.env.MK_ROLLBAR_ACCESS_TOKEN,
+  ROLLBAR_ENVIRONMENT: import.meta.env.MK_ROLLBAR_ENVIRONMENT,
 };
--- src/main.jsx
+++ src/main.jsx
@@ -7,13 +7,12 @@ import App from "./App.jsx";
 import "./assets/styles/normalize.scss";
 
 import "./assets/styles/main.scss";
-
+import { ENV } from './config/constants.js';
 
 const rollbarConfig = {
-  accessToken: '4bafbfa4fc9549368ecb7d93cefdc8f9',
-  environment: 'testenv',
-};
-
+  accessToken: ENV.ROLLBAR_ACCESS_TOKEN,
+  environment: ENV.ROLLBAR_ENVIRONMENT,
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>

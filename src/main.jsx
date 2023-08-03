c13bb7036dfcb0d398d98f97780227707d01ad37
Michael Nascimento <michael.nsc@outlook.com>
2023-08-03 07:53:19 -0300

    adiciona rollbar no front

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,6 @@
+import { ErrorBoundary, Provider as RollbarProvider } from '@rollbar/react';
 import React from "react";
+
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
@@ -6,8 +8,19 @@ import "./assets/styles/normalize.scss";
 
 import "./assets/styles/main.scss";
 
+
+const rollbarConfig = {
+  accessToken: '4bafbfa4fc9549368ecb7d93cefdc8f9',
+  environment: 'testenv',
+};
+
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RollbarProvider config={rollbarConfig}>
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
+    </RollbarProvider>
   </React.StrictMode>
 );

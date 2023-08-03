ddaf9e6e14a419b3fec77d60c657218a6becdb5a
Michael Nascimento <michael.nsc@outlook.com>
2023-08-03 08:56:11 -0300

    cria auth service

--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -4,4 +4,22 @@ import client from "./appwrite-service";
 
 const auth = new Account(client);
 
-export default auth;
+export const oauthLogin = () => {
+  auth.createOAuth2Session(
+    "google",
+    "http://localhost:5173",
+    "http://localhost:3000/auth-error",
+    [
+      "https://www.googleapis.com/auth/userinfo.email",
+      "https://www.googleapis.com/auth/userinfo.profile",
+    ]
+  );
+};
+
+export const clearSession = async () => {
+  await auth.deleteSession("current");
+};
+
+export const getCurrentSession = async () => {
+  return await auth.getSession("current");
+};
